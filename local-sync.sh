9d7625c6ad276fdc5de81d68089c7589732473ec
intelburn <=nerdacs@gmail.com>
2017-12-13 18:55:33 -0500

    Made pathing based on variables

--- local-sync.sh
+++ local-sync.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+$SYNC=
 #Use select structure to locate drive for use as destination
 echo "Select destination:"
 select drive in /media/$(whoami)/*/ ;
@@ -19,5 +20,5 @@ if [ "$subdir" = 'y' ] || [ "$subdir" = 'Y' ]; then
 	dest=${dest}_sync/
 fi
 #Perform an rsync with the arguments for using the checksum, display verbose info, recursively, delete items from the destination not in the source, and to exclude the hidden files made by BT Sync
-rsync -c -v -r --delete --exclude=.* /sync/ncc/ $dest
+rsync -c -v -r --delete --exclude=.* $SYNC/ $dest
 echo "Done"
