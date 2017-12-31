0ffa341e385776d5416ca72ca0e0aed84b990d40
intelburn <=nerdacs@gmail.com>
2017-12-31 16:08:08 -0500

    Updated names of files and added version of syncing at home of owners of Macs

--- debian-sync.sh
+++ debian-sync.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-$SYNC=
+
+#Set SYNC to where Resilio is downloading the files to
+SYNC=
 #Use select structure to locate drive for use as destination
 echo "Select destination:"
 select drive in /media/$(whoami)/*/ ;
@@ -7,18 +9,13 @@ do
 	dest=$drive
 	break
 done
-#Determine if an _sync directory is desired
-echo -n "Want _sync (y/N)? "
-read subdir
-if [ "$subdir" = 'y' ] || [ "$subdir" = 'Y' ]; then
-	#Test for an _sync in destination drive, if none is present create on
-	if [ ! -d ${dest}_sync ]; then
-		mkdir ${dest}_sync
-		echo "created"
-	fi
-	#set the destination to include an _sync
-	dest=${dest}_sync/
+#Test for an _sync in destination drive, if none is present create on
+if [ ! -d ${dest}_sync ]; then
+	mkdir ${dest}_sync
+	echo "created"
 fi
+#set the destination to include an _sync
+dest=${dest}_sync/
 #Perform an rsync with the arguments for using the checksum, display verbose info, recursively, delete items from the destination not in the source, and to exclude the hidden files made by BT Sync
 rsync -c -v -r --delete --exclude=.* $SYNC/ $dest
 echo "Done"
